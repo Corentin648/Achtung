a21cae5d1df93e22b31332f521f0f2b4e9eaea7a
Corentin Dominguez <corentin.dmz@live.fr>
2020-07-02 00:51:47 +0200

    gestion collisions avec trace

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,5 @@
 extends Node
-
+signal hit
 export (PackedScene) var Trace
 
 
@@ -12,18 +12,28 @@ func _ready():
 	pass
 
 
-func _process(delta):
+func _physics_process(delta):
 	var trace = Trace.instance()
 	add_child(trace)
 	trace.position = Vector2($Body.get_position().x,$Body.get_position().y)
-	$Body.move_and_slide(velocity)
+	$Body.set_position($Body.get_position() + velocity*delta)
 	if Input.is_action_pressed("ui_left"):
 		$Body.rotate(-PI/50)
-		velocity = velocity.rotated(-PI/50).normalized()*100
-		#$Body.move_and_slide(velocity)
+		velocity = velocity.rotated(-PI/50).normalized()*200
 	if Input.is_action_pressed("ui_right"):
 		$Body.rotate(PI/50)
-		velocity = velocity.rotated(PI/50).normalized()*100
-		#$Body.move_and_slide(velocity)
+		velocity = velocity.rotated(PI/50).normalized()*200
 	pass
 
+
+
+func _on_Body_input_event(viewport, event, shape_idx):
+	print("cest le test")
+	$Body/CollisionShape2D.set_deferred("disabled", true)
+	pass # Replace with function body.
+
+
+func _on_Body_body_entered(body):
+	velocity.x = 0
+	velocity.y = 0
+	pass # Replace with function body.
