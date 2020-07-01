9750b2d1ce6c06881c1bd27cdf8a4b49367ab835
Corentin Dominguez <corentin.dmz@live.fr>
2020-07-01 23:52:20 +0200

    début gestion collision

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -18,10 +18,12 @@ func _process(delta):
 	trace.position = Vector2($Body.get_position().x,$Body.get_position().y)
 	$Body.move_and_slide(velocity)
 	if Input.is_action_pressed("ui_left"):
-		velocity = velocity.rotated(-PI/30).normalized()*100
+		$Body.rotate(-PI/50)
+		velocity = velocity.rotated(-PI/50).normalized()*100
 		#$Body.move_and_slide(velocity)
 	if Input.is_action_pressed("ui_right"):
-		velocity = velocity.rotated(PI/30).normalized()*100
+		$Body.rotate(PI/50)
+		velocity = velocity.rotated(PI/50).normalized()*100
 		#$Body.move_and_slide(velocity)
 	pass
 
